2e0f7b9253bbbaf359560619d15229a2fa8d6602
Mauricio Miguel <maumiguel.dzovoniarkiewicz@gmail.com>
2023-12-04 14:23:51 -0300

    updating

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,121 @@
-const { series } = require('gulp')
-const gulp = require ('gulp')
-const concat = require ('gulp-concat')
-const cssmin = require ('gulp-cssmin')
-const rename = require ('gulp-rename')
-const uglify = require ('gulp-uglify')
-const image = require ('gulp-image')
+const { series, parallel} = require('gulp')
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const image = require('gulp-image')
+const stripJs = require('gulp-strip-comments')
+const stripCss = require('gulp-strip-css-comments')
 const htmlmin = require('gulp-htmlmin')
 const babel = require('gulp-babel')
 const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')( require('node-sass'))
+const { pipe } = require('stdout-stream')
+const { contains } = require('jquery')
+const { default: gulpStripCssComments } = require('gulp-strip-css-comments')
 const reload = browserSync.reload
 
 function tarefasCSS(cb) {
 
-    return gulp.src([
-        './node_modules/bootstrap/dist/css/bootstrap.css',
-        './vendor/owl/css/owl.css',
-        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
-        './vendor/jquery-ui/jquery-ui.min.css',
-        './src/css/style.css'
-        
-        
+    gulp.src([
+            './node_modules/bootstrap/dist/css/bootstrap.css',
+            './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+            './vendor/owl/css/owl.css',
+            './vendor/jquery-ui/jquery-ui.css'
         ])
-        .pipe(concat('styles.css'))
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'})) // libs.min.css
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(stripCss())                   // remove comentários css   
+        .pipe(concat('libs.css'))           // mescla arquivos
+        .pipe(cssmin())                     // minifica css
+        .pipe(rename({ suffix: '.min'}))    // libs.min.css
+        .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
 
-    return callback()
+    cb()
+
+}
+
+function tarefasSASS(cb) {
+    
+    gulp.src('./src/scss/**/*.scss')
+        .pipe(sass()) // transforma o sass para css
+        .pipe(gulp.dest('./dist/css')) 
+
+    cb()
 }
 
 function tarefasJS(callback){
 
     gulp.src([
-        './node_modules/jquery/dist/jquery.js',
-        './node_modules/bootstrap/dist/js/bootstrap.js',
-        './vendor/owl/js/owl.js',
-        './vendor/jquery-mask/jquery.mask.min.js',
-        './vendor/jquery-ui/jquery-ui.min.js',
-        './src/js/custom.js'
-    ])
-    .pipe(babel({
-        comments: false,
-        presets: ['@babel/env']
-    }))
-
-        .pipe(concat('scripts.js'))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min'})) //libs.min.js
-        .pipe(gulp.dest('./dist/js'))
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './vendor/owl/js/owl.js',
+            './vendor/jquery-mask/jquery.mask.js',
+            './vendor/jquery-ui/jquery-ui.js',
+            './src/js/custom.js'
+        ])
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('scripts.js'))         // mescla arquivos
+        .pipe(uglify())                     // minifica js
+        .pipe(rename({ suffix: '.min'}))    // scripts.min.js
+        .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
 
     return callback()
 }
 
-function tarefaImagem(callback){
-
-    gulp.src('./src/images/*')
+function tarefasImagem(){
+    
+    return gulp.src('./src/images/*')
         .pipe(image({
-        pngquant: true,
-        optipng: false,
-        zopflipng: true,
-        jpegRecompress: false,
-        mozjpeg: false,
-        gifsicle: true,
-        svgo: true,
-        concurrent: 10,
-        quiet: true
-    }))
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
         .pipe(gulp.dest('./dist/images'))
-
-    return callback()
 }
 
 // POC - Proof of Concept
-function tarefasHTML (callback){
+function tarefasHTML(callback){
 
     gulp.src('./src/**/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true}))
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./dist'))
 
     return callback()
+
 }
 
 gulp.task('serve', function(){
 
     browserSync.init({
-        server:{
-            baseDir: './dist'
+        server: {
+            baseDir: "./dist"
         }
     })
     gulp.watch('./src/**/*').on('change', process) // repete o processo quando alterar algo em src
     gulp.watch('./src/**/*').on('change', reload)
+
 })
 
 function end(cb){
-    console.log('Tarefa finalizada')
+    console.log("tarefas concluídas")
     return cb()
 }
 
-const process = series( tarefasHTML, tarefasJS, tarefasCSS, end)
+// series x parallel
+const process = parallel( tarefasHTML, tarefasJS, tarefasCSS, tarefasSASS, end)
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
-exports.images = tarefaImagem
+exports.images = tarefasImagem
+exports.sass = tarefasSASS
 
 exports.default = process
